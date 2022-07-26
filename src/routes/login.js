8072041425b40b971ecfbc6d3d4a01fa66d8163f
Thibaut Dusautoir <tdusautoir@myges.fr>
2022-07-26 18:13:22 +0200

    add auth with jwt

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,5 +1,7 @@
 const { User } = require("../db/sequelize");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const privateKey = require("../auth/private_key");
 
 module.exports = (app) => {
   app.post("/api/login", (req, res) => {
@@ -17,8 +19,13 @@ module.exports = (app) => {
               let message = "Le mot de passe est incorrect.";
               return res.status(401).json({ message });
             }
+
+            let token = jwt.sign({ userId: user.id }, privateKey, {
+              expiresIn: "24h",
+            });
+
             const message = `L'utilisateur a été connecté avec succès`;
-            return res.json({ message, data: user });
+            return res.json({ message, data: user, token });
           });
       })
       .catch((err) => {
